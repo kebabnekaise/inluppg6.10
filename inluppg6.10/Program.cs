79fb3be68808b1d5e45a78de5bda70e07e8a6cd1
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-03-01 10:20:15 +0100

    Add project files.

---
+++ inluppg6.10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace inluppg6_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en text. Du kommer att få hur många tal som finns i den");
+            string text = Console.ReadLine();
+            Console.WriteLine(AntalTalIText(text));
+        }
+
+        static int AntalTalIText(string text)
+        {
+            int konverteratSkrap;
+            float konverteratFloat = 0;
+            int cunt = 0;
+            string[] splitString = text.Split(' ');
+            foreach (string s in splitString)
+            {
+                if (int.TryParse(s, out konverteratSkrap))
+                {
+                    cunt++;
+                }
+                else if (float.TryParse(s, out konverteratFloat))
+                {
+                    cunt++;
+                }
+            }
+            return cunt;
+        }
+    }
+}
